26a8c0ff4192d0741d1af042a871c05936fda3c5
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-27 19:57:56 +0530

    prime number

--- Prime_number.java
+++ Prime_number.java
@@ -1,5 +1,23 @@
+import java.util.*;
 public class Prime_number {
+    static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        else{
+            for(int i = 2; i*i <= n; i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to check: ");
+        int num = sc.nextInt();
+        System.err.println(isPrime(num));
     }
 }
