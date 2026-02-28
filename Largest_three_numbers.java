131d71bf5a9e5835327a58277f7d31e698a739a9
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-28 09:43:48 +0530

    largest of three number

---
+++ Largest_three_numbers.java
@@ -0,0 +1,11 @@
+public class Largest_three_numbers {
+    public static void main(String [] args){
+        int a = 2;
+        int b = 4;
+        int c = 5;
+        
+        int max = Math.max(a, Math.max(b,c));
+        System.out.println(max);
+    }
+
+}
