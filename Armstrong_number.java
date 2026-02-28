a747544f8873b0aae2a2694c6def049d89c27ed4
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-28 09:22:35 +0530

    armstrong

---
+++ Armstrong_number.java
@@ -0,0 +1,15 @@
+public class Armstrong_number {
+    public static void main(String[] args) {
+
+        int n = 1634;
+        int temp = n;
+        int sum = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n = n / 10;
+        }
+System.out.println(temp == sum ? "Armstrong" : "Not Armstrong");        
+
+    }
+}
