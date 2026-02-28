257a74bb7de6119a03709853716eb433c784dee5
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-28 09:11:23 +0530

    check palindrome

--- check_palindrome_number.java
+++ check_palindrome_number.java
@@ -1,12 +1,15 @@
 public class check_palindrome_number {
     public static void main(String[] args) {
-        int n = 231213;
-        int temp = n, rev = 0;
+        int n = 12321;
+        int temp = n;
+        int rev = 0;
         while (n != 0) {
-            rev = rev *10;
-            rev += n /10;
+            rev = rev*10;
+            rev += n%10;
             n = n/10;
         }
+
+        // System.out.print(rev);
         System.out.print(temp == rev ? "Palindrome" : "Not Palindrome");
     }
 }
