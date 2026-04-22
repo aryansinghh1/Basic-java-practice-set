d6362bd7f6d7ff5a3b5f8312f3b6779a8f924c5a
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-04-22 15:04:04 +0530

    printing odd place

---
+++ Character_odd_place.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Character_odd_place {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your string: ");
+        
+
+        String s = sc.nextLine();
+
+        for(int i = 0; i<s.length(); i++){
+
+            if(i%2!= 0){
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
+ 
