d7cd7155963ba3b84382bd08f50d32f21e16fdff
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-04-22 17:08:06 +0530

    fibonacci test

--- Test_level.java
+++ Test_level.java
@@ -3,30 +3,23 @@ import java.util.*;
 public class Test_level {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your String to check: ");
-        String s = sc.nextLine();
-        s = s.toLowerCase();
+        System.out.print("Enter your number to check: ");
+        int num = sc.nextInt();
 
-        String[] arr = s.split(" ");
+        int a = 0;
+        
+        int b = 1;
 
-        int count = 0;
+        
 
-        String sb;
-
-        for (int i = 0; i < arr.length; i++) {
-            char ch = arr[i].charAt(0);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'
-                    || ch == 'u') {
-                count++;
-            }
-
+        while(num>0){
+            System.out.print(a + " ");
+            int temp = a +b;
+            a = b;
+            b = temp;
+            num--;
         }
-        if (count > 0) {
-            System.out.print(count);
-        } else {
-            System.out.print("No such words");
-
-        }
+        
 
     }
 }
