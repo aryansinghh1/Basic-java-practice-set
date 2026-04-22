4bca1b22304cdd75384c62b0e88a899d572aa22b
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-04-22 14:11:27 +0530

    testing

---
+++ Test_level.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Test_level {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number to check: ");
+        int num = sc.nextInt();
+
+        int digit = String.valueOf(num).length();
+        int temp = num;
+        int sum = 0;
+
+        for(int i = 0; i < digit; i++){
+            int num2 = num%10;
+            sum += Math.pow(num2,digit);
+            num = num/10;
+
+        }
+
+        if(temp == sum){
+            System.out.print("its armstrong number");
+        }
+        else{
+            System.out.print("it is not armstrong number");
+
+        }
+
+
+    }
+}
