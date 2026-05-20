bdac35cd6402d2dd53e8005e7ff828547b0fa881
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-05-20 15:04:07 +0530

    counting words

--- Practice.java
+++ Practice.java
@@ -2,18 +2,27 @@
 import java.util.Scanner;
 
 public class Practice {
-    public static void main(String [] args){
-        System.out.print("Enter the string: \n");
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String string1 = sc.nextLine();
-
-        for(int i = 0 ; i< string1.length(); i++){
-            if(i%2 != 0){
-                System.out.print(string1.charAt(i));
+        String s = sc.nextLine().toLowerCase();
+        String[] str = s.split(" ");
+        int count = 0;
+        for (int i = 0; i < str.length; i++) {
+            for (int j = 1; j < str[i].length(); j++) {
+                if (isVowel(str[i].charAt(j - 1)) == true && isVowel(str[i].charAt(j)) == true) {
+                    count++;
+                    break;
+                }
             }
         }
-
-        
+        System.out.println(count);
     }
-    
+
+    public static boolean isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return true;
+        }
+        return false;
+    }
+
 }
