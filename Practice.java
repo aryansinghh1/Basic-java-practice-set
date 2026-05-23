3ae4dd9609ccefef85074413297b685e608102ec
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-05-23 16:13:38 +0530

    count number of words in a string

--- Practice.java
+++ Practice.java
@@ -8,45 +8,22 @@ public class Practice {
         Scanner sc = new Scanner(System.in);
 
         String str = sc.nextLine();
-
-        int u = 0;
-        int l = 0;
+        String [] arr = str.split(" ");
+        
         int c = 0;
-        int v = 0;
-        int num = 0;
+      
 
-        for (int i = 0; i < str.length(); i++) {
-
-            if (Character.isLetter(str.charAt(i))) {
-                if (Character.isUpperCase(str.charAt(i))) {
-                    u++;
-                } else if (Character.isLowerCase(str.charAt(i))) {
-                    l++;
-                }
-
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != ""){
+                c++;
             }
-            if (Character.isLetter(str.charAt(i))) {
-                if (str.charAt(i) == 'a' || str.charAt(i) == 'o' || str.charAt(i) == 'u' || str.charAt(i) == 'i'
-                        || str.charAt(i) == 'e' || str.charAt(i) == 'A' || str.charAt(i) == 'E'
-                        || str.charAt(i) == 'I'
-                        || str.charAt(i) == 'O' || str.charAt(i) == 'U') {
-                    v++;
 
-                } else {
-                    c++;
-                }
-            }
-            if (Character.isDigit(str.charAt(i))) {
-                num++;
-            }
 
         }
 
-        System.out.println("count of Uppercase: " + u);
-        System.out.println("count of Lowercase: " + l);
-        System.out.println("count of vowel: " + v);
-        System.out.println("count of consonent: " + c);
-        System.out.println("count of number: " + num);
+        
+        System.out.println("count of word: " + c);
+       
 
     }
 
