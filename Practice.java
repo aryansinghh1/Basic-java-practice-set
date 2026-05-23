5c2317d7966749bd5cbd7397ee77acb15de32e84
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-05-23 16:00:21 +0530

    counting vowel consonent upper lowercase and number in a string

--- Practice.java
+++ Practice.java
@@ -9,19 +9,45 @@ public class Practice {
 
         String str = sc.nextLine();
 
-        int count = 0;
-        
+        int u = 0;
+        int l = 0;
+        int c = 0;
+        int v = 0;
+        int num = 0;
 
         for (int i = 0; i < str.length(); i++) {
 
-            if (Character.isUpperCase(str.charAt(i))) {
-                count++;
+            if (Character.isLetter(str.charAt(i))) {
+                if (Character.isUpperCase(str.charAt(i))) {
+                    u++;
+                } else if (Character.isLowerCase(str.charAt(i))) {
+                    l++;
+                }
+
             }
-                
+            if (Character.isLetter(str.charAt(i))) {
+                if (str.charAt(i) == 'a' || str.charAt(i) == 'o' || str.charAt(i) == 'u' || str.charAt(i) == 'i'
+                        || str.charAt(i) == 'e' || str.charAt(i) == 'A' || str.charAt(i) == 'E'
+                        || str.charAt(i) == 'I'
+                        || str.charAt(i) == 'O' || str.charAt(i) == 'U') {
+                    v++;
+
+                } else {
+                    c++;
+                }
+            }
+            if (Character.isDigit(str.charAt(i))) {
+                num++;
+            }
+
         }
 
-        System.out.println("count of Uppercase: " + count);
-        
+        System.out.println("count of Uppercase: " + u);
+        System.out.println("count of Lowercase: " + l);
+        System.out.println("count of vowel: " + v);
+        System.out.println("count of consonent: " + c);
+        System.out.println("count of number: " + num);
+
     }
 
 }
