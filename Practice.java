6a58a6dc15cdedc3a9bb730b36fc54989a0972b1
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-05-25 15:59:27 +0530

    print first letter of each word in a string

--- Practice.java
+++ Practice.java
@@ -4,33 +4,27 @@ public class Practice {
 
     public static void main(String[] args) {
 
-        //count number of string
+        //print first letter of each word in a string
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter long string: ");
         String str = sc.nextLine();
-        System.out.println("enter string to count: ");
-        String str1 = sc.nextLine();
+    
 
-        String[] astr = str.split(" ");
+        String[] arr = str.split(" ");
         String ch = "";
 
-        int count = 0;
-
-        for (int i = 0; i < astr.length; i++) {
-            for(int j = 0 ; j < astr[i].length(); j++){
-                if(Character.isLetter(astr[i].charAt(j))){
-                    ch = ch + astr[i].charAt(j);
-                }
+        for(int i = 0 ;i<arr.length; i++){
+            if(Character.isLetter(arr[i].charAt(0))){
+                ch = ch + arr[i].charAt(0);
             }
-            if(ch.equals(str1)){
-                count++;
-            }
-            ch = "";
         }
+        
+            
+        
 
-        System.out.println(count);
+        System.out.println(ch);
     }
 
 }
