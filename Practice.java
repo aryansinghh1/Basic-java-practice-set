2b657e2d6848e9d921ed97f1b04d78a88fbd47a6
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-05-25 15:26:59 +0530

    fibonacci

--- Practice.java
+++ Practice.java
@@ -4,31 +4,26 @@ public class Practice {
 
     public static void main(String[] args) {
 
-        // count the word in a sentence that starts with vowel
-
-        System.out.println("Enter Your string: ");
+        // fibonacci
+        System.out.println("Enter your number: ");
         Scanner sc = new Scanner(System.in);
 
-        String str = sc.nextLine();
-        String[] arr = str.split(" ");
+        int num = sc.nextInt();
+        int a = 0;
+        int b = 1;
 
-        int c = 0;
+        for (int i = 0; i < num; i++) {
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].charAt(0) == 'a' || arr[i].charAt(0) == 'e' || arr[i].charAt(0) == 'i' || arr[i].charAt(0) == 'o'
-                    || arr[i].charAt(0) == 'u' || arr[i].charAt(0) == 'A' || arr[i].charAt(0) == 'E'
-                    || arr[i].charAt(0) == 'I' || arr[i].charAt(0) == 'O' || arr[i].charAt(0) == 'U') {
-                c++;
+            System.out.println(a);
+            int temp = a + b;
+            a = b;
+            b = temp;
+
+            if(a>num){
+                break;
             }
 
-        }
 
-        if (c > 0) {
-
-            System.out.println("count of word: " + c);
-        } else {
-
-            System.out.println("No such words");
         }
 
     }
