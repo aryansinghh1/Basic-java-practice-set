4d40bd76febb08e9b4749875b54087553d56cd9c
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-27 20:41:23 +0530

    gcd of multiple numbers

--- Practice.java
+++ Practice.java
@@ -2,28 +2,29 @@ import java.util.Scanner;
 
 public class Practice {
 
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number for gcd: ");
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        
-        while(num2!=0){
-            int temp = num2;
-            num2 = num1 % num2;
-            num1 = temp;
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the numbers:");
+
+        int gcd = sc.nextInt(); // First number
+
+        for (int i = 1; i < n; i++) {
+            int num = sc.nextInt();
+
+            while (num != 0) {
+                int temp = num;
+                num = gcd % num;
+                gcd = temp;
+            }
         }
-        while (num3 != 0){
-            int temp = num3;
-            num3 = num1%num3;
-            num1 = temp;
-        }
-    
-        System.out.println(num1);
+
+        System.out.println("GCD = " + gcd);
+
         sc.close();
     }
 }
