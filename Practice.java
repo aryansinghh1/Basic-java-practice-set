f49ea9ef6f77659d7321af959d15135324fee62f
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-27 20:36:24 +0530

    gcd of three numbers

--- Practice.java
+++ Practice.java
@@ -2,43 +2,28 @@ import java.util.Scanner;
 
 public class Practice {
 
-    // Function outside main
-    public static int[] twoSum(int[] arr, int target) {
-
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-
-                if (arr[i] + arr[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-
-        return new int[]{-1, -1};
-    }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter size of array: ");
-        int num = sc.nextInt();
-
-        int[] arr = new int[num];
-
-        System.out.println("Enter array elements:");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
+        System.out.println("Enter the number for gcd: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        
+        while(num2!=0){
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
         }
-
-        System.out.print("Enter target: ");
-        int target = sc.nextInt();
-
-        // Calling the function
-        int[] result = twoSum(arr, target);
-
-        System.out.println("Indices: " + result[0] + " " + result[1]);
-
+        while (num3 != 0){
+            int temp = num3;
+            num3 = num1%num3;
+            num1 = temp;
+        }
+    
+        System.out.println(num1);
         sc.close();
     }
 }
