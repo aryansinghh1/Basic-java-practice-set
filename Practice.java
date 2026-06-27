21e9e609bec95c34bf93a228fdfb73f00426f54e
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-06-27 20:24:24 +0530

    two sum

--- Practice.java
+++ Practice.java
@@ -2,29 +2,43 @@ import java.util.Scanner;
 
 public class Practice {
 
-    public static void main(String[] args) {
+    // Function outside main
+    public static int[] twoSum(int[] arr, int target) {
 
-        // find the first longest non repeated character substring
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter Your Number: ");
-
+        System.out.print("Enter size of array: ");
         int num = sc.nextInt();
-        
-        int a = 0;
-        int b = 1;
-        while(num!=0){
-            System.out.print(a+" ");
-            int temp = a;
-            a = a+b;
-            b = temp;
-            num--;
+
+        int[] arr = new int[num];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-        
-        
 
-        
+        System.out.print("Enter target: ");
+        int target = sc.nextInt();
+
+        // Calling the function
+        int[] result = twoSum(arr, target);
+
+        System.out.println("Indices: " + result[0] + " " + result[1]);
+
+        sc.close();
     }
-
 }
